a2651ceccc8965daf1b673c27b84a11614d30dbc
MMiyauchi <masaki@miyauchi.me>
2020-04-24 12:40:15 +0900

    Feature/c (#1)
    * ファイル移動
    
    * fix
    
    * add normal_sum.c
    
    * fix
    
    * Add recursive_sum
    
    Co-authored-by: Masaki Miyauchi <m.miyauchi@sonas.co.jp>

---
+++ c/recursive_sum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+const long int NUM = 100000;
+
+long int calcSum(long int num) {
+  long int sum = 0;
+  if (num == 0) {
+    return 0;
+  }
+  return (num + calcSum(num -1));
+} 
+
+int main()
+{
+  clock_t start, end;
+  start = clock();
+  long int result = 0;
+
+  for (long int i = 1; i <= NUM; i++)
+  {
+    result = result + calcSum(i);
+  }
+
+  end = clock();
+  printf("NUM: %ld, result: %ld \n", NUM, result);
+  printf( "time: %lu ms", end - start);
+  return 0;
+}
